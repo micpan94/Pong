d9ea3efcf2e9c735579b78405486d4f906b70b61
Michał Pankiewicz <rasiak94@gmail.com>
2019-05-15 10:28:11 +0200

    Add new files

---
+++ Pong by Paneq/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Pong_by_Paneq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.koniecgry = new System.Windows.Forms.Label();
+            this.punkty = new System.Windows.Forms.Label();
+            this.wynik = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.koniecgry);
+            this.playground.Controls.Add(this.punkty);
+            this.playground.Controls.Add(this.wynik);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(864, 482);
+            this.playground.TabIndex = 0;
+            // 
+            // koniecgry
+            // 
+            this.koniecgry.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.koniecgry.AutoSize = true;
+            this.koniecgry.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.koniecgry.Location = new System.Drawing.Point(411, 23);
+            this.koniecgry.Name = "koniecgry";
+            this.koniecgry.Size = new System.Drawing.Size(441, 228);
+            this.koniecgry.TabIndex = 4;
+            this.koniecgry.Text = "Koniec Gry\r\n\r\nF1 - Zacznij od Nowa \r\nEsc - Wyjdz ";
+            // 
+            // punkty
+            // 
+            this.punkty.AutoSize = true;
+            this.punkty.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.punkty.Location = new System.Drawing.Point(186, 23);
+            this.punkty.Name = "punkty";
+            this.punkty.Size = new System.Drawing.Size(47, 57);
+            this.punkty.TabIndex = 3;
+            this.punkty.Text = "0";
+            this.punkty.Click += new System.EventHandler(this.punkty_Click);
+            // 
+            // wynik
+            // 
+            this.wynik.AutoSize = true;
+            this.wynik.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wynik.ForeColor = System.Drawing.Color.Black;
+            this.wynik.Location = new System.Drawing.Point(23, 23);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(171, 57);
+            this.wynik.TabIndex = 2;
+            this.wynik.Text = "Wynik :";
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(162, 154);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Black;
+            this.racket.Location = new System.Drawing.Point(401, 435);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(200, 20);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 482);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label punkty;
+        private System.Windows.Forms.Label wynik;
+        private System.Windows.Forms.Label koniecgry;
+    }
+}
+
---
+++ Pong by Paneq/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong_by_Paneq
+{
+    public partial class Form1 : Form
+    {
+
+        public int speed_left = 4;   // predkosc kuli
+        public int speed_top = 4;
+        public int point = 0; // punkty :D
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide(); // Hide the cursor
+
+            this.FormBorderStyle = FormBorderStyle.None; // usuwamy ramki
+            this.TopMost = true;                  // wyciaga formsa
+            this.Bounds = Screen.PrimaryScreen.Bounds;   // Pełny ekran
+
+            racket.Top = playground.Bottom - (playground.Bottom / 10); // pozycja rakiety
+
+            koniecgry.Left = (playground.Width / 2) - (koniecgry.Width / 2);
+            koniecgry.Top = (playground.Height / 2) - (koniecgry.Height / 2);
+            koniecgry.Visible = false; 
+           
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            racket.Left = Cursor.Position.X - (racket.Width / 2); // ustawiamy pozycje rakiety wzgledem kursora 
+
+            ball.Left += speed_left; // poruszanie piłki
+            ball.Top += speed_top;
+
+
+            if(ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left && ball.Right <= racket.Right) // kolizje
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top; // zmiana kierunku 
+                point += 1;
+
+                punkty.Text = point.ToString();
+
+            }
+
+            if (ball.Left <= playground.Left)
+            {
+
+                speed_left = -speed_left; 
+            }
+            if(ball.Right >= playground.Right)
+            {
+
+                speed_left = -speed_left;
+            }
+            if(ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+
+            }
+            if (ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false; // piłka wyszła koniec gry
+                koniecgry.Visible = true; // koniec gry wyswietlnie napisu 
+            }
+
+
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { this.Close(); } //esc do wyjscia
+            if (e.KeyCode == Keys.F1)
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                point = 0;
+                punkty.Text = "0";
+                timer1.Enabled = true;
+                koniecgry.Visible = false;
+            }
+
+
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void punkty_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
